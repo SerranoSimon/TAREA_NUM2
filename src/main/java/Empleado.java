a7004577b774896c0efafc95dd65cd6327535477
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-12 19:52:56 -0400

    Avance Clase Empleado

--- src/main/java/Empleado.java
+++ src/main/java/Empleado.java
@@ -3,5 +3,22 @@ public class Empleado {
     private String apellidos;
     private String nombre;
     private String correo;
-
+    public Empleado(String id, String apellidos, String nombre, String correo){
+        this.id=id;
+        this.apellidos=apellidos;
+        this.nombre=nombre;
+        this.correo=correo;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getApellidos(){
+        return apellidos;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getCorreo(){
+        return correo;
+    }
 }
