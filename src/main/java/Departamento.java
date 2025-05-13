1a3e787598aa7c4694d4572e08a92b413e201f77
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-12 21:22:59 -0400

    Avance Clase Departamento

--- src/main/java/Departamento.java
+++ src/main/java/Departamento.java
@@ -1,6 +1,15 @@
 public class Departamento {
     private String nombre;
-    public int obtenerCantidadEmpleados(){
-        return 0; //por mientras
+    public Departamento(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int obtenerCantidadEmpleados() {
+        return 0;
     }
 }
