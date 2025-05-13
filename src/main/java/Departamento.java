055a46d642d4198e86aa5e36e1e1928a0a1ce826
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-12 22:57:49 -0400

    Edición Clase Departamento

--- src/main/java/Departamento.java
+++ src/main/java/Departamento.java
@@ -1,15 +1,17 @@
+import java.util.ArrayList;
+
 public class Departamento {
     private String nombre;
+    private ArrayList<Empleado> empleados =new ArrayList<>();
+
     public Departamento(String nombre) {
         this.nombre = nombre;
     }
     public String getNombre() {
         return nombre;
     }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+
     public int obtenerCantidadEmpleados() {
-        return 0;
+        return empleados.size();
     }
 }
