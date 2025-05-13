ce339d9481fe138af10824c6bdc370a20b5e52de
SerranoSimon <sserrano2024@inf.udec.cl>
2025-05-12 20:52:44 -0600

    ReunionVirtual y ReunionPresencial extienden Reunion

--- src/main/java/ReunionPresencial.java
+++ src/main/java/ReunionPresencial.java
@@ -1,3 +1,12 @@
-public class ReunionPresencial {
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ReunionPresencial extends Reunion {
     private String sala;
+
+    public ReunionPresencial(Date fecha, Instant horaPrevista, Duration duracionPrevista, ArrayList<Invitacion> invitados) {
+        super(fecha, horaPrevista, duracionPrevista, invitados);
+    }
 }
--- src/main/java/ReunionVirtual.java
+++ src/main/java/ReunionVirtual.java
@@ -1,3 +1,12 @@
-public class ReunionVirtual {
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ReunionVirtual extends Reunion{
     private String enlace;
+
+    public ReunionVirtual(Date fecha, Instant horaPrevista, Duration duracionPrevista, ArrayList<Invitacion> invitados) {
+        super(fecha, horaPrevista, duracionPrevista, invitados);
+    }
 }
