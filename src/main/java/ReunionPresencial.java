80b9b6b82ac38890e3a58042387da44766289713
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-16 17:36:33 -0400

    Escritura javadoc Clase ReunionPresencial

--- src/main/java/ReunionPresencial.java
+++ src/main/java/ReunionPresencial.java
@@ -2,10 +2,22 @@ import java.io.IOException;
 import java.time.*;
 import java.util.ArrayList;
 
-
+/**
+ *Clase que hereda de Reunion, representa una reunión presencial.
+ */
 public class ReunionPresencial extends Reunion {
     private String sala;
 
+    /**
+     *
+     * @param tipoReunion tipo de reunion (MARKETING, TECNICA, OTRO)
+     * @param fecha fecha de la reunion
+     * @param horaPrevista hora a la que debería comenzar la reunión.
+     * @param duracionPrevista duración estimada de la reunión.
+     * @param sala sala en la que se hará la reunión.
+     * @param organizador empleado organizador de la reunión.
+     * @throws IOException Si ocurre un error relacionado a la creación del archivo de notas.
+     */
     public ReunionPresencial(tipoReunion tipoReunion,LocalDate fecha, Instant horaPrevista, Duration duracionPrevista,String sala, Organizador organizador) throws IOException {
         super(tipoReunion,fecha, horaPrevista, duracionPrevista, organizador);
         this.sala=sala;
