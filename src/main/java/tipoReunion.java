9c24dc0a3ab598c532d7ec44a8c195dfcedda59b
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-16 18:51:33 -0400

    Escritura javadoc Clase tipoReunion

--- src/main/java/tipoReunion.java
+++ src/main/java/tipoReunion.java
@@ -1,3 +1,7 @@
+/**
+ * Enum que presenta los distintos tipos de reunión.
+ * Se presentan tres posibles casos, reunión técnnica, de marketing u otro.
+ */
 public enum tipoReunion {
     TECNICA,
     MARKETING,
