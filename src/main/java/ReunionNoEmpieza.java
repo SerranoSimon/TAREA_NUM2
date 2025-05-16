3bf2aa75179a53293734056d66448da05dcc625b
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-15 23:19:14 -0400

    Escritura javadoc ReunionNoEmpieza

--- src/main/java/ReunionNoEmpieza.java
+++ src/main/java/ReunionNoEmpieza.java
@@ -1,4 +1,11 @@
+/**
+ * Excepción que indica que una reunión todavía no ha comenzado.
+ */
 public class ReunionNoEmpieza extends Exception {
+    /**
+     *Indica que la reunión todavía no inicia con un mensaje personalizado.
+     * @param message mensaje personalizado que indica que la reunión aún no ha empezado.
+     */
     public ReunionNoEmpieza(String message) {
         super(message);
     }
