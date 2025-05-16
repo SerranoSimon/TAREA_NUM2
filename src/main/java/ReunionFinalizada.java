9d195092e3f1323d4d3ae4defe3a0da913f120ed
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-15 23:16:51 -0400

    Escritura javadoc ReunionFinalizada

--- src/main/java/ReunionFinalizada.java
+++ src/main/java/ReunionFinalizada.java
@@ -1,4 +1,11 @@
+/**
+ * Excepción que representa el fin de una reunión.
+ */
 public class ReunionFinalizada extends Exception {
+    /**
+     *Indica que la reunión ha finalizado con un mensaje personalizado.
+     * @param message mensaje personalizado que indica que la reunión ha terminado.
+     */
     public ReunionFinalizada(String message) {
         super(message);
     }
