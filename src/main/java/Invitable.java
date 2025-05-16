6d8b897763fc93d474eba2550914c56f1185a7b5
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-16 13:59:38 -0400

    Escritura javadoc Invitable

--- src/main/java/Invitable.java
+++ src/main/java/Invitable.java
@@ -1,5 +1,12 @@
 import java.time.Instant;
 
+/**
+ *Interfaz que representa la capacidad de un empleaod para ser invitado a una reunión.
+ */
 public interface Invitable {
+    /**
+     *permite la invitación de un empleado a une reunión.
+     * @param invitacion invitación a la reunión.
+     */
     public void invitar(Invitacion invitacion);
 }
