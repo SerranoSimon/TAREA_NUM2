e4fdccab6328a54be48aa3062d08e8519c6fb760
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-05-23 14:32:12 -0400

    Creación Test Departamento metodo invitar()

---
+++ src/test/java/DepartamentoTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartamentoTest {
+
+    @Test
+    void invitar() throws ReunionFinalizada, ReunionNoEmpieza, IOException {
+        Organizador o1 = new Organizador("101","Mora Muñoz","Pedro","pedromora@gmail.com");
+        Empleado e1 = new Empleado("102","Chavez Hernandez","Carlos","carloschavez@gmail.com");
+        Empleado e2 = new Empleado("103","Chavez Zapata","Maria","mariachavez@gmail.com");
+        Reunion r1 = new ReunionPresencial(tipoReunion.TECNICA, LocalDate.now(), Instant.parse("2025-05-13T08:17:00Z"),1.5f ,"A1",o1);
+        Departamento d1 = new Departamento("Departamento B");
+        d1.addEmpleado(e1);
+        d1.addEmpleado(o1);
+        d1.addEmpleado(e2);
+
+        d1.invitar(r1.getInvitacion());
+
+        ArrayList<Persona> listaEsperada = new ArrayList<>();
+        listaEsperada.add(o1);
+        listaEsperada.add(e1);
+        listaEsperada.add(e2);
+        assertEquals(listaEsperada,r1.getInvitacion().getListaDeInvitados());
+
+    }
+}
